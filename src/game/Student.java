cd48806c2eb79b3c866af64f9a2623edbf30ca86
Supermarcel10 <47798787+Supermarcel10@users.noreply.github.com>
2023-02-19 16:23:38 +0000

    - Changed Student to extend Walker.

--- src/game/Student.java
+++ src/game/Student.java
@@ -3,7 +3,7 @@ package game;
 import city.cs.engine.*;
 
 
-public class Student extends DynamicBody {
+public class Student extends Walker {
 	private static final Shape STUDENT_SHAPE = new BoxShape(1,2);
 	private static final BodyImage IMAGE = new BodyImage("/home/marcel/Projects/IntelliJ/javaproject2023-Supermarcel10/data/img/student.png", 4);
 	private int credits = 0;
