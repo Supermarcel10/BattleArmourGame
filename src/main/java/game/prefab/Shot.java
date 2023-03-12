c01c9cc7638ec48e964a47558ff744d131434fd8
Marcel <marcelbarlik@gmail.com>
2023-03-12 05:09:37 +0000

    - Added movement smoothing to the Player - Refactored Pellet.java to Shot.java

--- src/main/java/game/prefab/Shot.java
+++ src/main/java/game/prefab/Shot.java
@@ -5,17 +5,22 @@ import game.objects.abstractBody.Body;
 import org.jbox2d.common.Vec2;
 
 
-public class Pellet extends Body {
-	public Pellet(float speed, World world, Vec2 position) {
+public class Shot extends Body {
+	private static final float speed = 10f;
+	private Vec2 travelDirection;
+
+	public Shot(float speed, World world, Vec2 position, Vec2 travelDirection) {
 		super(speed, world, position);
+		this.travelDirection = travelDirection;
 		spawn();
 	}
 
 	@Override
 	public void spawn() {
 		// TODO: Get player direction and make the pellet move in that direction.
-		setMoveDirection(new Vec2(0, 0));
+		setMoveDirection(travelDirection);
 		body = new DynamicBody(world, new CircleShape(0.5f * scaleFactor, position));
+		// TODO: Fix stuff with the ghostly fixture.
 		GhostlyFixture fixture =  new GhostlyFixture(body, new CircleShape(0.5f * scaleFactor, position));
 		fixture.setDensity(0f);
 		body.setFillColor(java.awt.Color.RED);
