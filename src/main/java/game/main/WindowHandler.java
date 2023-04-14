d86ea0c46aaf207589a6fe4b66f936ea71de929d
Marcel <marcelbarlik@gmail.com>
2023-04-14 19:20:30 +0100

    - Updated and optimised WindowHandler

--- src/main/java/game/main/WindowHandler.java
+++ src/main/java/game/main/WindowHandler.java
@@ -5,6 +5,7 @@ import city.cs.engine.UserView;
 import city.cs.engine.World;
 import game.input.Config;
 import org.jbox2d.common.Vec2;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
@@ -16,9 +17,12 @@ import static game.main.Game.*;
 
 public class WindowHandler {
 	private static final World world = Game.world;
-	private static JLayeredPane layeredPane;
+	private static JLayeredPane layeredPane = new JLayeredPane();
+	private static JPanel overlayPanel = new JPanel();
 	public static final JFrame frame = new JFrame(Config.title);
-	public static UserView view = null;
+	public static UserView view = new UserView(world, 0, 0);
+
+	private static Font font;
 
 	private static JButton[] menuButtons;
 	private static JPanel menuPanel;
@@ -26,19 +30,36 @@ public class WindowHandler {
 
 	public static JLabel scoreLabel;
 
+	private static void loadFont() {
+		try {
+			// Open & register the font with the GraphicsEnvironment
+			Font ttf = Font.createFont(Font.TRUETYPE_FONT, new File(Config.font.get("default")));
+			GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(ttf);
+			font = ttf.deriveFont(Font.PLAIN, Math.round(scaleFactor * 60));
+		} catch (IOException | FontFormatException e) {
+			font = new Font("Algerian", Font.BOLD, Math.round(scaleFactor * 60));
+		}
+	}
+
+	private static @NotNull JLabel createText(String text, @NotNull JPanel panel) {
+		JLabel label = new JLabel(text, SwingConstants.CENTER);
+		label.setFont(font);
+		label.setForeground(Color.WHITE);
+		label.setOpaque(false); // Transparent
+		panel.add(label);
+
+		return label;
+	}
 
 	public static void createWindow(World world) {
-		// Create a user view with the world and resolution provided.
-		view = new UserView(world, 0, 0);
-
+		loadFont();
 		updateWindow();
 
-		layeredPane = new JLayeredPane();
 		layeredPane.setPreferredSize(view.getPreferredSize());
 		layeredPane.setOpaque(false); // Set the background color to be transparent
 
 		// Add the view to the layered pane at the bottom layer with an explicit position and size.
-		layeredPane.add(view, 1);
+		layeredPane.add(view, JLayeredPane.FRAME_CONTENT_LAYER);
 		view.setBounds(0, 0, view.getPreferredSize().width, view.getPreferredSize().height);
 
 		// Add the layered pane to the frame.
@@ -50,6 +71,11 @@ public class WindowHandler {
 		frame.pack();
 		frame.setVisible(true);
 		view.requestFocus();
+
+		// OVERLAY PANEL
+		overlayPanel.setOpaque(false);
+		layeredPane.add(overlayPanel, JLayeredPane.PALETTE_LAYER);
+		overlayPanel.setSize(view.getPreferredSize());
 
 		// Enable debugs.
 		if (Config.DEBUG) {
@@ -153,32 +179,8 @@ public class WindowHandler {
 	}
 
 	public static void createGameOverlay() {
-		Font font;
-		try {
-			// Open & register the font with the GraphicsEnvironment
-			Font ttf = Font.createFont(Font.TRUETYPE_FONT, new File(Config.font.get("default")));
-			GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(ttf);
-			font = ttf.deriveFont(Font.PLAIN, Math.round(scaleFactor * 60));
-		} catch (IOException | FontFormatException e) {
-			font = new Font("Algerian", Font.BOLD, Math.round(scaleFactor * 60));
-		}
-
-		// Create a new JPanel to hold the score label.
-		JPanel overlayPanel = new JPanel();
-		overlayPanel.setOpaque(false); // Transparent
-
 		// Create a new JLabel for the score and add it to the overlay panel.
-		scoreLabel = new JLabel("SCORE: " + score, SwingConstants.CENTER);
-		scoreLabel.setFont(font);
-		scoreLabel.setForeground(Color.WHITE);
-		scoreLabel.setOpaque(false); // Transparent
-		overlayPanel.add(scoreLabel);
-
-		// Add the overlay panel to the layered pane at a higher layer than the UserView.
-		layeredPane.add(overlayPanel, JLayeredPane.PALETTE_LAYER);
-
-		// Set the size of the overlay panel to match the size of the UserView.
-		overlayPanel.setSize(view.getPreferredSize().width, scoreLabel.getPreferredSize().height + 10);
+		scoreLabel = createText("SCORE: " + score, overlayPanel);
 
 		// Adjust the bounds of the scoreLabel to avoid being clipped by the overlayPanel.
 		scoreLabel.setBorder(BorderFactory.createEmptyBorder(5, 1, 1, 1));
