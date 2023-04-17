23d2d7c0b846da6b58fd3812365a1cad287858bf
Marcel <marcelbarlik@gmail.com>
2023-04-17 13:44:43 +0100

    - Removed redundant code. - Fixed spawners immediately being destroyed when spawned. - Fixed spawners not aligned properly to their positions.

--- src/main/java/game/prefab/Spawn.java
+++ src/main/java/game/prefab/Spawn.java
@@ -20,37 +20,40 @@ public class Spawn extends Body {
 	public Spawn(@NotNull TankType type, Vec2 pos) {
 		super(pos);
 
-		spawners.add(this);
+		// Normalise the position to the grid.
+		setPosition(getJBoxPos());
 
 		// Create a new ghostly fixture
-		new GhostlyFixture(this, new CircleShape(scaleFactor * 2));
+		new GhostlyFixture(this, new CircleShape(2f));
 
 		// Change properties
 		setFillColor(java.awt.Color.WHITE);
-		setGravityScale(0);
-		setClipped(true);
 
 		if (type == TankType.PLAYER) {
 			Player player = type.createPlayer(pos);
 			int numOfPlayers = (int) Arrays.stream(Game.player).filter(Objects::nonNull).count();
 			Game.player[numOfPlayers] = player;
+
+			removeSpawner();
 		} else {
+			spawners.add(this);
+
 			// Spawn enemy after 1 second.
 			new Timer(1000, e -> {
 				Enemy enemy = type.createEnemy(pos);
 				enemy.spawn();
 				enemies.add(enemy);
 
+				removeSpawner();
+
 				// Stop after first execution to allow for GC.
 				((Timer) e.getSource()).stop();
 			}).start();
 		}
-
-		this.destroy();
-		spawners.remove(this);
 	}
 
-	public Spawn(@NotNull TankType type, float x, float y) {
-		this(type, new Vec2(x, y));
+	private void removeSpawner() {
+		this.destroy();
+		spawners.remove(this);
 	}
 }
