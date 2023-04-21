062789047341455b122f5bc5d559e7ce404212a0
Marcel <marcelbarlik@gmail.com>
2023-04-21 16:26:18 +0100

    - Set the default location of the file chooser

--- src/main/java/game/window/WindowBuild.java
+++ src/main/java/game/window/WindowBuild.java
@@ -16,6 +16,11 @@ public class WindowBuild {
 	protected static JLabel lblExitBuildMode;
 
 	public static void createBuildOverlay() {
+		// Set the default starting directory to the project directory and file name to NAME.level
+		fileChooser.setSelectedFile(new File("NAME.level"));
+		fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir") + "\\src\\main\\resources\\levels"));
+
+		// Create the overlay, hide old labels.
 		if (lblScore != null) lblScore.setVisible(false);
 
 		lblPlacementBlock = createText(String.valueOf(BlockType.BRICK), 1, pnlOverlay, 1);
