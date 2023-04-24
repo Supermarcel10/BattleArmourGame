ea291c6baed6d7f41062bde1ef42dd9b11018133
Marcel <marcelbarlik@gmail.com>
2023-04-24 21:14:27 +0100

    - Fixed issue with pickup spawning in wrong location.

--- src/main/java/game/objects/pickup/Pickup.java
+++ src/main/java/game/objects/pickup/Pickup.java
@@ -20,7 +20,7 @@ public class Pickup extends Body implements SensorListener {
 		super(0f);
 		this.type = type;
 
-		setPositionJBox(position);
+		setPosition(position);
 
 		// Add to pickups list.
 		pickups.add(this);
