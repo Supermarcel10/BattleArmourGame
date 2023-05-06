af8119705f3d4eacbb380d597e64628dc540a6a2
Supermarcel10 <marcelbarlik@gmail.com>
2023-05-06 11:39:46 +0100

    - Fixed Pickup not always picking up.

--- src/main/java/game/objects/pickup/Pickup.java
+++ src/main/java/game/objects/pickup/Pickup.java
@@ -8,15 +8,17 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
-import static game.MainGame.pickups;
-import static game.MainGame.scaleFactor;
+import static game.MainGame.*;
 import static game.objects.tank.Tank.halfSize;
 
 
 /**
  * Pickup class for pickup perks.
  */
-public class Pickup extends DynamicBody implements SensorListener {
+public class Pickup extends DynamicBody implements StepListener {
+	private static final int CHECK_RATE = 6;
+	private static int untilCheck = 0;
+
 	private static final Shape shape = new CircleShape((halfSize / 1.5f) * scaleFactor);
 	public final PickupType type;
 
@@ -31,12 +33,11 @@ public class Pickup extends DynamicBody implements SensorListener {
 
 		// Create a new ghostly fixture
 		new GhostlyFixture(this, shape);
-		Sensor sensor = new Sensor(this, shape);
 
 		this.addImage(new BodyImage(type.image, halfSize * 2));
 
-		// Add collision listener.
-		sensor.addSensorListener(this);
+		// Add step listener.
+		world.addStepListener(this);
 
 		// Add a timer to destroy the pickup after 10 seconds.
 		new Timer(10000, e -> {
@@ -55,18 +56,26 @@ public class Pickup extends DynamicBody implements SensorListener {
 		pickups.remove(this);
 	}
 
-	/**
-	 * Pickup collision listener.
-	 * @param e SensorEvent.
-	 */
 	@Override
-	public void beginContact(@NotNull SensorEvent e) {
-		// TODO: Fix this sometimes not registering
-		if (e.getContactBody() instanceof Player p) {
-			p.pickUp(this);
-			destroy();
-		}
+	public void preStep(StepEvent stepEvent) {
+		if (untilCheck == 0) {
+			for (Player p : player) {
+				System.out.println("Checking if player is in range");
+				if (p == null || p.health <= 0) continue;
+
+				Vec2 pos = getPosition();
+				Vec2 playerPos = p.getPosition();
+
+				if (isWithinDistance(pos, playerPos, 0.75f)) {
+					System.out.println("Player is in range");
+					destroy();
+					p.pickUp(this);
+				}
+			}
+
+			untilCheck = CHECK_RATE;
+		} else untilCheck--;
 	}
 
-	@Override public void endContact(SensorEvent sensorEvent) {}
+	@Override public void postStep(StepEvent stepEvent) {}
 }
