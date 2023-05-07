fa1ad2769c622deb58e74dcdf4d96de00bd7857e
Supermarcel10 <marcelbarlik@gmail.com>
2023-05-07 03:52:23 +0100

    - Removed redundant Config options. - Removed DEBUG_DRAWs.

--- src/main/java/game/IO/Config.java
+++ src/main/java/game/IO/Config.java
@@ -1,6 +1,7 @@
 package game.IO;
 
 import org.jbox2d.common.Vec2;
+import org.jetbrains.annotations.ApiStatus;
 
 import java.awt.event.KeyEvent;
 import java.util.HashMap;
@@ -10,18 +11,15 @@ import java.util.HashMap;
  * Config class containing all user settings.
  */
 public class Config {
-	private static final String CD = System.getProperty("user.dir") + "/src/main/resources/";
-	public static final boolean DEBUG = true;
-	public static final boolean DEBUG_DRAW = false;
-
 	public static float soundVolume = 0.2f, musicVolume = 0.4f;
 
-	public static boolean fullscreen = false;
+	public @ApiStatus.Experimental static boolean fullscreen = false;
 //	public static Vec2 resolution = new Vec2(1280, 720);
 //	public static Vec2 resolution = new Vec2(1920, 1080);
 	public static Vec2 resolution = new Vec2(2560, 1440);
+
 	public static int fps = 60;
-	public static String title = "Battle Armour";
+
 	public static HashMap<String, Integer> key_binding = new HashMap<>(){{
 		put("player1-UP", KeyEvent.VK_W);
 		put("player1-LEFT", KeyEvent.VK_A);
